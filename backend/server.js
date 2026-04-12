29f37feb03f0bdc196431cec2ba9043b27b48ab0
Momoz-17 <mohitvijaygupta17@gmail.com>
2026-04-12 16:59:47 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'https://finance-tracker-1-qawj.onrender.com', 
+  origin: 'https://finance-tracker-1-qawj.onrender.com/auth', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
