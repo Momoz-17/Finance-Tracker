d776a5dea531d6b3048920c0b42b486a70ef957b
Momoz-17 <mohitvijaygupta17@gmail.com>
2026-04-12 20:54:45 +0530

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // Your backend runs on 5000, your frontend on 5173
+  // This is your Node (Backend) URL + /api
   baseURL: 'https://finance-tracker-i7lz.onrender.com/api', 
 });
 
